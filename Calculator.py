d7cd61cf6097da707803a9217d247a3fa6bb136e
Aditya Prasad <a.prasad.college@gmail.com>
2026-02-09 12:26:38 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -3,7 +3,7 @@ import addition as a
 import Subtraction as s
 import Multiplication as m
 
+#printing different functions
 print("add",a.add(9,5))
 print("sub",s.sub(9,5))
 print("mult",m.mul(9,5))
-
