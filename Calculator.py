6add0a8fed8b4755d3e0e7277a85ae767a6261e7
Aditya Prasad <a.prasad.college@gmail.com>
2026-02-09 12:31:24 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -4,6 +4,6 @@ import Subtraction as s
 import Multiplication as m
 
 #printing different functions
-print("add",a.add(9,5))
-print("sub",s.sub(9,5))
-print("mult",m.mul(9,5))
+print("add 9+5",a.add(9,5))
+print("sub 9-5",s.sub(9,5))
+print("mult 9*5",m.mul(9,5))
