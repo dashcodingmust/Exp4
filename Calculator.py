6b6363ae90b070c0949b923a66984f517a8f641d
aditya <a.prasad.college@gmail.com>
2026-02-09 12:40:19 +0530

    updating calculator

--- Calculator.py
+++ Calculator.py
@@ -2,8 +2,11 @@
 import addition as a
 import Subtraction as s
 import Multiplication as m
+import division as d
 
 #printing different functions
 print("add 9+5",a.add(9,5))
 print("sub 9-5",s.sub(9,5))
 print("mult 9*5",m.mul(9,5))
+print("mult 10/5",d.div(10,5))
+
